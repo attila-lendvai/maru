8efa78620bc6cf119d26586f75b1da254275b239
Ian Piumarta <ian@piumarta.com>
2012-09-04 08:35:35 +0900

    include size and slignment of all standard types and wchar; define RTLD_NOW and _GLOBAL

--- mkosdefs.c
+++ mkosdefs.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <wchar.h>
+
+#if defined(__WIN32__)
+# include <w32dlfcn.h>
+#else
+# include <dlfcn.h>
+#endif
 
 #define STR(X)		#X
 
 #define defint(X)	printf("(define %s %d)\n", #X, X)
 #define defstr(X)	printf("(define %s \"%s\")\n", #X, STR(X))
+#define defsize(X)	printf("(define-constant sizeof-%s %d)\n", #X, sizeof(X))
+#define defalign(X)	{ struct { char _;  X x; } x;  printf("(define-constant alignof-%s %d)\n", #X, (long)&x.x - (long)&x); }
+#define defsao(X)	defsize(X);  defalign(X)
+
+typedef long long longlong;
+typedef long double longdouble;
+typedef void *pointer;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef wchar_t wchar;
 
 int main()
 {
@@ -34,5 +53,19 @@ int main()
 # ifdef __linux__
     defint(__linux__);
 # endif
+    defsao(char);
+    defsao(short);
+    defsao(int);
+    defsao(long);
+    defsao(longlong);
+    defsao(int32);
+    defsao(int64);
+    defsao(wchar);
+    defsao(float);
+    defsao(double);
+    defsao(longdouble);
+    defsao(pointer);
+    defint(RTLD_NOW);
+    defint(RTLD_GLOBAL);
     return 0;
 }
