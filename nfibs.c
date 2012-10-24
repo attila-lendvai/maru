7879368e12321be35da959a02789e0ba19ce239b
Ian Piumarta <ian@piumarta.com>
2012-10-24 19:39:05 +0900

    call nfibs(40) once

--- nfibs.c
+++ nfibs.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-int fibs(int n)
+int fibs(long n)
 {
   return (n < 2) ? 1 : (fibs(n - 1) + fibs(n - 2) + 1);
 }
 
 int main()
 {
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
-  printf("%d\n", fibs(35));
+  printf("%d\n", fibs(40));
+  return 0;
 }
