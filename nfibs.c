1e25c2deda66364ef8165d0b3a7b987bc494176b
Ian Piumarta <ian@piumarta.com>
2012-12-04 09:56:17 +0900

    take arguments from the command line

--- nfibs.c
+++ nfibs.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int fibs(long n)
 {
   return (n < 2) ? 1 : (fibs(n - 1) + fibs(n - 2) + 1);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-  printf("%d\n", fibs(40));
-  return 0;
+    while (--argc) {
+	int arg= atoi(*++argv);
+	printf("nfibs(%d) = %d\n", arg, fibs(arg));
+    }
+    return 0;
 }
