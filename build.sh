a9d641aebf634547fb776fe61f271fe5ae367cde
Attila Lendvai <attila.lendvai@gmail.com>
2014-02-27 20:00:02 +0600

    work on build, now the standalone exe starts swank and waits for a connection in case of errors.

--- build.sh
+++ build.sh
@@ -19,20 +19,29 @@ fi
 # make quicklisp available below in the lisp code.
 # and get a fresh enough ASDF also.
 if [ ! -d build ]; then
+    mkdir -p ${SCRIPT_DIR}/build/quicklisp/local-projects/
+
     #
     # get a recent enough ASDF
     #
-    mkdir -p ${SCRIPT_DIR}/build/quicklisp/local-projects/
     cd ${SCRIPT_DIR}/build/quicklisp/local-projects/
     git clone git://common-lisp.net/projects/asdf/asdf.git
     cd asdf
     git checkout 3.1.0.65
     make
-    cd ../../
+
+    #
+    # get a recent slime/swank
+    #
+    cd ${SCRIPT_DIR}/build/quicklisp/local-projects/
+    git clone https://github.com/slime/slime.git
+    cd slime
+    git checkout e44a4c0038ff456a65a495eb4a81f9cb4c53f79f
 
     #
     # get quicklisp and load/install it under build/
     #
+    cd ${SCRIPT_DIR}/build/
     wget http://beta.quicklisp.org/quicklisp.lisp
     ${LISP} --no-userinit --no-sysinit --load quicklisp.lisp --eval "(quicklisp-quickstart:install :path \"${SCRIPT_DIR}/build/quicklisp\")" --quit
 fi
@@ -70,7 +79,21 @@ exec ${LISP} --no-sysinit --no-userinit --script "$0" --end-toplevel-options $@
 (format t "Lips side speaking, will (asdf:load-system :maru) now and dump an executable image~%")
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (asdf:load-system :maru)
+  ;; quicklisp doesn't hook into :defsystem-depends-on (?), so we need to quickload it explicitly
+  (ql:quickload :hu.dwim.logger)
+  (ql:quickload :maru+hu.dwim.logger)
+  (asdf:load-system :swank)
+  (ql:quickload :com.dvlsoft.clon))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  ;; this is an arbitrary list of pretty much everything. without this swank would try to load them when connecting
+  ;; which would not work.
+  (swank:swank-require (append '("SWANK-TRACE-DIALOG" "SWANK-PACKAGE-FU" "SWANK-ARGLISTS" "SWANK-REPL" "SWANK-SPROF"
+                                 "SWANK-CLIPBOARD" "SWANK-PRESENTATIONS" "SWANK-INDENTATION" "SWANK-FUZZY" "SWANK-C-P-C"
+                                 "SWANK-FANCY-INSPECTOR" "SWANK-UTIL" "SB-CLTL2"
+                                 "SWANK-ASDF" "asdf" "ASDF")
+                               #+sbcl
+                               '("SB-SPROF" "SB-GROVEL" "SWANK-SBCL-EXTS")))
   (ql:quickload :com.dvlsoft.clon))
 
 (in-package :maru.eval)
@@ -78,61 +101,116 @@ exec ${LISP} --no-sysinit --no-userinit --script "$0" --end-toplevel-options $@
 (eval-when (:execute :load-toplevel :compile-toplevel)
   (com.dvlsoft.clon:nickname-package))
 
-(setf uiop:*image-entry-point* 'maru-toplevel)
+(defun invoke-slime-debugger (condition previous-hook)
+  (declare (ignore previous-hook))
+  (maru.error "There was an error: ~A" condition)
+  (maru.error "Waiting for a slime/swank connection to present the error in the slime debugger. Press C-c to exit.")
+  (loop
+    :until (or swank::*emacs-connection*
+               (swank::default-connection))
+    :do
+    (write-string "." *debug-io*)
+    (finish-output *debug-io*)
+    (sleep 1))
+  (let (#+sbcl(sb-debug:*stack-top-hint* (sb-di:top-frame)))
+    (swank:swank-debugger-hook condition nil)))
+
+(defun install-debugger-hook ()
+  (declare (optimize debug))
+  #+sbcl ;; --script also disables the debugger, so let's turn it back on
+  (sb-ext:enable-debugger)
+  (setf *debugger-hook* 'invoke-slime-debugger)
+  #+nil ;; TODO delme?
+  (progn
+    (sb-ext:disable-debugger)
+    (setf sb-ext:*invoke-debugger-hook* invoke-slime-debugger)))
 
 (clon:defsynopsis (:postfix "FILES...")
   (text :contents
-	"Common Lisp implementation of maru.")
-  (group (:header "Flags (non valued options):")
-    (flag :short-name "h" :long-name "help"
-	  :description "Print this help and exit.")
-    (flag :short-name "v" ;; :long-name ""
-	  :description "ignored, only here for compatibility")
-    (flag :short-name "b" ;; :long-name ""
-	  :description "Don't try to load boot.l")
-    (flag :short-name "g" ;; :long-name ""
-	  :description "ignored, only here for compatibility")
-    (flag :short-name "O" ;; :long-name ""
-	  :description "ignored, only here for compatibility")))
+        "Common Lisp implementation of maru.")
+  (group (:header "Flags:")
+         (flag :short-name "h" :long-name "help"
+               :description "Print this help and exit.")
+         (flag :short-name "v" ;; :long-name ""
+               :description "ignored, only for compatibility")
+         (flag :short-name "b" ;; :long-name ""
+               :description "Don't try to load boot.l")
+         (flag :short-name "g" ;; :long-name ""
+               :description "ignored, only for compatibility")
+         (flag :short-name "O" ;; :long-name ""
+               :description "ignored, only for compatibility"))
+  (group (:header "Options with arguments:")
+         (stropt :long-name "swank-port"
+                 :argument-name "4005"
+                 :description "Start swank on this port, defaulting to 4005. Zero means don't start swank.")))
 
-(defun maru-toplevel ()
-  (with-new-maru-state
-    (flet
-        ((build-maru-args-list ()
-           (let ((args (clon:cmdline))
-                 (maru/args (maru/intern "nil")))
-             (dolist (arg (reverse (rest args)))
-               (setf maru/args (maru/cons arg maru/args)))
-             (maru/define (global-namespace-of *eval-context*)
-                          (maru/intern "*arguments*")
-                          maru/args)))
-         (load-boot ()
-           (maru/repl #P"boot.l")))
-      (let ((args-var (build-maru-args-list))
-            (load-boot t)
-            (repled nil))
-        (loop
-          :for cell = (maru/get-var args-var) :then (maru/cdr cell)
-          :until (maru/nil? cell)
-          :for arg = (maru/car cell)
-          :do
-          (cond
-            ((member arg '("-v" "-g" "-O") :test 'equal)
-             ;;noop
-             )
-            ((equal arg "-b")
-             (setf load-boot nil))
-            (t
-             (when load-boot
-               (load-boot))
-             ;; NOTE the arg voodoo in eval.c is not mirrored perfectly here
-             (maru/set-var args-var (maru/get-tail cell))
-             (maru/repl (pathname arg))
-             (setf repled t))))
-        (unless repled
-          (when load-boot
-            (load-boot))
-          (maru/repl *terminal-io* :output-stream *terminal-io* :prompt ".")
-          (format t "morituri te salutant~%"))))))
+(defun toplevel ()
+  (restart-case
+      (let ((swank-port 4005)
+            (logger (hu.dwim.logger:find-logger 'maru)))
+        (setf (hu.dwim.logger:log-level/runtime logger) hu.dwim.logger:+info+)
+        (clon:make-context)
+        (when (or (clon:getopt :long-name "help")
+                  (not (clon:cmdline-p)))
+          (clon:help)
+          (clon:exit))
+        #+sbcl
+        (flet ((signal-handler (signal code scp)
+                 (declare (ignore signal code scp))
+                 (format *error-output* "~%SIGTERM/SIGINT was received, initiating shutdown~%")
+                 (uiop:quit)))
+          (sb-sys:enable-interrupt sb-unix:sigterm #'signal-handler)
+          (sb-sys:enable-interrupt sb-unix:sigint #'signal-handler))
+        (awhen (clon:getopt :long-name "swank-port")
+          (setf swank-port (parse-integer it :junk-allowed t))
+          (unless swank-port
+            (error "Invalid swank port ~S" it)))
+        (unless (zerop swank-port)
+          (maru.debug "Starting a swank server on port ~S" swank-port)
+          (let ((swank::*loopback-interface* "127.0.0.1"))
+            (swank:create-server :port swank-port
+                                 :style :spawn
+                                 :dont-close t)))
+        (install-debugger-hook)
+        (maru.debug "Remainder arguments are ~S" (clon:remainder))
+        (with-new-maru-state
+          (let ((load-boot? (not (clon:getopt :short-name "b")))
+                (repled nil))
+            (flet
+                ((build-maru-args-list ()
+                   (let ((args (clon:remainder))
+                         (maru/args (maru/intern "nil")))
+                     (dolist (arg (reverse args))
+                       (setf maru/args (maru/cons arg maru/args)))
+                     (maru/define (global-namespace-of *eval-context*)
+                                  (maru/intern "*arguments*")
+                                  maru/args)))
+                 (maybe-load-boot ()
+                   (when load-boot?
+                     (maru.debug "Loading boot.l")
+                     (maru/repl #P"boot.l")
+                     (setf load-boot? nil))))
+              (loop
+                :with args-var = (build-maru-args-list)
+                :for cell = (maru/get-var args-var) :then (maru/cdr cell)
+                :until (maru/nil? cell)
+                :for arg = (maru/car cell)
+                :do (progn
+                      (maybe-load-boot)
+                      ;; NOTE the arg voodoo in eval.c is not mirrored perfectly here
+                      (maru/set-var args-var (maru/get-tail cell))
+                      (maru.debug "About to REPL file ~S" arg)
+                      (maru/repl (pathname arg))
+                      (setf repled t)))
+              (unless repled
+                (maybe-load-boot)
+                (maru.debug "Entering interactive REPL on *terminal-io*")
+                (maru/repl *terminal-io* :output-stream *terminal-io* :prompt ".")
+                (format t "morituri te salutant~%"))))))
+    (abort ()
+      :report "Exit the lisp vm."
+      (uiop:die 42 "Game over."))))
+
+(setf uiop:*image-entry-point* 'toplevel)
 
 (uiop:dump-image "maru" :executable t)
