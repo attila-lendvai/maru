9ff7fcfddecf4a8428b75d2b7e625bfd42238f12
Attila Lendvai <attila@lendvai.name>
2021-03-01 13:52:11 +0100

    added default.nix

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs            ? import <nixpkgs> {},
+  pkgsx86_64Linux ? import <nixpkgs> { system = "x86_64-linux"; },
+  pkgsi686Linux   ? import <nixpkgs> { system = "i686-linux"; }
+}:
+
+# https://nixos.wiki/wiki/Packaging/32bit_Applications
+#pkgs.mkShell {
+#pkgs.stdenv.mkDerivation {
+#pkgsi686Linux.stdenv.mkDerivation {
+#pkgs.clangMultiStdenv.mkDerivation {
+pkgs.multiStdenv.mkDerivation {
+  name = "maru";
+
+  buildInputs = with pkgs;
+    [
+      coreutils time ncurses posix_man_pages bash-completion less
+      #lorri direnv
+      gitFull diffutils
+      gnumake
+      libffi.dev
+      gdb binutils
+      llvm_11 clang_11
+
+      # keep this line if you use bash
+      bashInteractive
+    ];
+
+  # buildCommand = ''
+  #   make eval
+  # '';
+}
