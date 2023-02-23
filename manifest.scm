3dc15e4db56cbe4138d6de6857e7dbfecfa52f45
Attila Lendvai <attila@lendvai.name>
2023-02-23 15:30:42 +0100

    build: add a manifest.scm for guix
    manifest.scm contains the packages needed for development. you can
    issue a `guix shell --pure` to enter an isolated environment for
    compilation.
    
    on traditional linux distros you can just install the necessary
    packages, or alternatively install guix, the package manager.
    
    Change-Id: I7cef22daccdbe1cc4c9a2f620b96f1859f6f8c81

---
+++ manifest.scm
@@ -0,0 +1,42 @@
+;; This is the complete list of Guix packages necessary for building Maru.
+;;
+;; Usage:
+;;
+;; guix shell --pure
+;; guix shell --pure -- make test
+
+;; TODO -m32 does not work as of 2025-06:
+;; cc -O3 -m32 -o build/x86-libc/i386-linux-gnu/eval1  build/x86-libc/i386-linux-gnu/eval1.s
+;; ld: cannot find crtbeginS.o: No such file or directory
+;; ld: cannot find -lgcc: No such file or directory
+;; ld: cannot find -lgcc_s: No such file or directory
+;; ld: skipping incompatible /gnu/store/hw6g2kjayxnqi8rwpnmpraalxi0djkxc-glibc-2.39/lib/libc.so when searching for -lc
+;; ld: cannot find -lc: No such file or directory
+;; ld: skipping incompatible /gnu/store/hw6g2kjayxnqi8rwpnmpraalxi0djkxc-glibc-2.39/lib/libc.so when searching for -lc
+;; ld: cannot find -lgcc: No such file or directory
+;; ld: cannot find -lgcc_s: No such file or directory
+;; ld: cannot find crtendS.o: No such file or directory
+
+(specifications->manifest
+ '("coreutils"
+   "diffutils"
+   "clang-toolchain"
+   "glibc"
+   ;; "libffi"
+   "make"
+   "time"
+   "ncurses" ; for clear, reset, tput (used by the Makefile)
+   "git"
+   "findutils" ; find is used in the Makefile
+
+   ;;
+   ;; the rest is only for convenience
+   ;;
+   "bash-completion"
+   "gawk"
+   "sed"
+   "git:gui"
+   "man-pages"
+   "less"
+   "gdb"
+   ))
